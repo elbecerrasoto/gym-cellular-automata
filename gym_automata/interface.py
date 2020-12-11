f3b9d4e718da4f790fd6da382ba8f395f44e8df0
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2020-12-10 18:16:42 -0700

    docs: interface docstring

--- gym_automata/interface.py
+++ gym_automata/interface.py
@@ -12,30 +12,35 @@ gym_automata_doc = \
     OPERATOR objects
     1. Automaton
     2. Modifier
-    
     operator.update(grid, action, state)
+    returns: grid
     
     WRAPPER objects
     1. CAEnv  
     """
-
+    
 # ---------------- Data Classes
 class Grid:
-    """DATA object 
-    
+    """DATA object gym_automata
+    Grid(shape, cell_states, data, cell_type)
+        
     It represents the grid (spatial disposition of cells) of a Cellular Automaton.
+    Access the data as a numpy array.
+    
+    When building your own CA environments usually this Class is used directly,
+    without extra customization.
     
     A Grid data object needs four pieces of information
-    1. data (optional, random sampled if not provided)
-    2. shape (multidimensional shape, usually 2-D)
-    3. cell_states (n number of cell states, they will be labeled from 0 to n-1)
-    4. cell_type (optional, default=np.int32)
+    1. shape (tuple) multidimensional shape, usually 2-D
+    2. cell_states (int) n number of cell states, they will be labeled from 0 to n-1
+    3. data (array like) optional, random sampled if not provided
+    4. cell_type (type) optional, default=np.int32
     
     e.g. grid of 8x8 with 2 cell states and random initialization
     grid = Grid(shape=(8,8), cell_states=2)
     
     e.g. grid of 8x8 with 2 cell states and custom initialization
-    grid_ones = Grid(data=np.ones((8,8)), shape=(8,8), cell_states=2)
+    grid_ones = Grid(shape=(8,8), cell_states=2, data=np.ones((8,8)))
     
     Access the data as a numpy array
     grid[:]
@@ -45,7 +50,9 @@ class Grid:
     grid.grid_space.contains(grid_ones[:])
     grid_ones.grid_space.contains(grid[:])
     """
-    def __init__(self, data=None, shape=None, cell_states=None, cell_type=np.int32):
+    __doc__ += gym_automata_doc
+
+    def __init__(self, shape=None, cell_states=None, data=None, cell_type=np.int32):
         assert not(shape is None or cell_states is None), 'shape and cell_states must be explicitly provided.'
         self.grid_space = spaces.Box(low=0, high=cell_states-1, shape=shape, dtype=cell_type)
         if data is None:
@@ -65,19 +72,24 @@ class Grid:
         return self.data.__repr__()
 
 class State:
-    """DATA object
+    """DATA object gym_automata
+    State(data, state_space)
     
-    It represents the hidden state of the modifier.
+    It represents the current state of the modifier.
     Used to store information need by
-    the Modifier to update the grid and
-    the CAEnv to calculate Reward and Termination
+    the Modifier to update the grid and posibly by
+    the environment to calculate reward and termination.
+    
+    When building your own CA environments usually this Class is used directly,
+    without extra customization.
     
     A State data object needs two pieces of information
-    1. data
-    2. space (a Gym Space type)
+    1. data (object) state data to hold
+    2. space (gym space) space which the data belongs
     
     Declare a space
     e.g. A discrete space from 0 to 7
+    from gym import spaces
     state_space = spaces.Discrete(8)
     
     State data must be explicitly provided
@@ -90,6 +102,8 @@ class State:
     Access the data by its __call__() method
     state()
     """
+    __doc__ += gym_automata_doc
+
     def __init__(self, data=None, state_space=None):
         if data is None and state_space is None:
             self.data = None
@@ -117,14 +131,19 @@ class State:
 
 # ---------------- Operator Classes
 class Automaton:
-    """OPERATOR object
+    """OPERATOR object gym_automata
     
     It operates over a grid,
     representing a 1-step computation of a Cellular Automaton.
     
+    When building your own CA environments this Class must be customized, usually by
+    inheritance, to build your own Cellular Automaton.
+
     Its main method update performs
     a 1-step update of the grid by following the Automaton rules and neighborhood.
     """
+    __doc__ += gym_automata_doc
+
     # Set these in ALL subclasses
     grid_space = None
 
@@ -141,13 +160,20 @@ class Automaton:
         raise NotImplementedError
 
 class Modifier:
-    """OPERATOR object    
+    """OPERATOR object gym_automata
+    
     It operates over a grid,
     represeting some sort of control task over it.
     
-    Its main method is update, which receives a grid, and action and a current mofifier state
+    When building your own CA environments this Class must be customized, usually by
+    inheritance, to build your own Modifier, that controls the dynamics of a CA
+    by changing its grid according to the taken action and its current state.
+    
+    Its main method is update, which receives a grid, and action and a modifier state
     and returns a grid.
     """
+    __doc__ += gym_automata_doc
+
     # Set these in ALL subclasses
     grid_space = None
     action_space = None
@@ -159,7 +185,7 @@ class Modifier:
         Args:
             grid (grid): a grid provided by the environment
             action (object): an action provided by the agent
-            state (state): modifier internal state, if any
+            state (state): modifier state, if any
         Returns:
             grid (grid): modified grid
         """
@@ -167,10 +193,15 @@ class Modifier:
 
 # ---------------- Wrapper Classes
 class CAEnv:
-    """WRAPPER object
+    """WRAPPER object gym_automata
     Wraps the data objects and the operator objects of gym-automata library into a
     coherent OpenAI Gym Environment.
+    
+    When building your own CA environments this Class is usually inherited by your
+    final Environment Class, together with the Gym.Env Class.
     """
+    __doc__ += gym_automata_doc
+
     # Set these in ALL subclasses
     # Data
     grid = None
