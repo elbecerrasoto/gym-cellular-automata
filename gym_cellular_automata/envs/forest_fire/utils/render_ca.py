d835013199a51959cbfe0d6bcc0c7095591794b0
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2021-02-08 21:01:34 -0700

    FF Render & its test

--- gym_cellular_automata/envs/forest_fire/utils/render_ca.py
+++ gym_cellular_automata/envs/forest_fire/utils/render_ca.py
@@ -1,62 +1,74 @@
-def render(self, title='Forest Fire Automaton'):
-    grid = self.grid_to_rgba()
-    """Automaton visualization"""
+from operator import itemgetter
+
+import numpy as np
+import matplotlib.pyplot as plt 
+from matplotlib import colors 
+import seaborn as sns
+
+from .config import get_forest_fire_config_dict
+
+CONFIG = get_forest_fire_config_dict()
+
+EMPTY = CONFIG['cell_symbols']['empty']
+TREE = CONFIG['cell_symbols']['tree']
+FIRE = CONFIG['cell_symbols']['fire']
+SYMBOLS = EMPTY, TREE, FIRE
+
+def plot_grid(grid, title = CONFIG['plot_title'], **kwargs):
+    n_row, n_col = grid.shape
+    
+    default_kwargs = {'title_font': {'fontname': CONFIG['plot_font']},
+                    'axes_font': {'fontname': CONFIG['plot_font']},        
+                    'color_empty': CONFIG['cell_colors']['empty'],
+                    'color_tree':  CONFIG['cell_colors']['tree'],
+                    'color_fire':  CONFIG['cell_colors']['fire']}
+
+    kwargs = {**default_kwargs, **kwargs}
+    
+    title_font = kwargs['title_font']
+    axes_font = kwargs['axes_font']
+    
+    color_empty = kwargs['color_empty']
+    color_tree  = kwargs['color_tree']
+    color_fire  = kwargs['color_fire']
+    colors_forest = color_empty, color_tree, color_fire
+
+    symbols_with_colors = zip(SYMBOLS, colors_forest)
+    symbols_with_colors_sorted_by_symbol = sorted(symbols_with_colors, key = itemgetter(0))
+    symbols, colors_forest = zip(*symbols_with_colors_sorted_by_symbol)
+
+    # Mapping from Color to Cell Symbols
+    cmap_colors = colors.ListedColormap(colors_forest)
+    norm_symbols = colors.BoundaryNorm([0, 1, 2, 3], 3)
+
     # Plot style
     sns.set_style('whitegrid')
+    
     # Main Plot
-    plt.imshow(grid, aspect='equal')
+    plt.imshow(grid, aspect='equal', cmap=cmap_colors, norm=norm_symbols)
+    
     # Title showing Reward
-    plt.title(title, **self.title_font)
+    plt.title(title, **title_font)
+    
     # Modify Axes
     ax = plt.gca()
+    
     # Major ticks
-    ax.set_xticks(np.arange(0, self.n_col, 1))
-    ax.set_yticks(np.arange(0, self.n_row, 1))
+    ax.set_xticks(np.arange(0, n_col, 1))
+    ax.set_yticks(np.arange(0, n_row, 1))
+    
     # Labels for major ticks
-    ax.set_xticklabels(np.arange(0, self.n_col, 1), **self.axes_font)
-    ax.set_yticklabels(np.arange(0, self.n_row, 1), **self.axes_font)
+    ax.set_xticklabels(np.arange(0, n_col, 1), **axes_font)
+    ax.set_yticklabels(np.arange(0, n_row, 1), **axes_font)
+    
     # Minor ticks
-    ax.set_xticks(np.arange(-.5, self.n_col, 1), minor=True)
-    ax.set_yticks(np.arange(-.5, self.n_row, 1), minor=True)
+    ax.set_xticks(np.arange(-.5, n_col, 1), minor=True)
+    ax.set_yticks(np.arange(-.5, n_row, 1), minor=True)
+    
     # Gridlines based on minor ticks
     ax.grid(which='minor', color='whitesmoke', linestyle='-', linewidth=2)
     ax.grid(which='major', color='w', linestyle='-', linewidth=0)
     ax.tick_params(axis=u'both', which=u'both',length=0)
-    fig = plt.gcf()
-    plt.show()
+
+    fig = plt.gcf()  
     return fig
-    
-
-
-
-
-# Render Info
-title_font = {'fontname':'Comfortaa'}
-axes_font = {'fontname':'Comfortaa'}
-alpha = int(1.0*255)
-color_tree = np.array([15, 198, 43, alpha]) # Green RGBA
-color_empty = np.array([255, 245, 166, alpha]) # Beige RGBA
-color_fire = np.array([255, 106, 58, alpha]) # Red RGBA
-color_rock = np.array([179, 139, 109, alpha]) # Brown RGBA
-color_lake = np.array([131, 174, 255, alpha]) # Blue RGBA
-
-
-
-def grid_to_rgba(self):
-    rgba_mat = self.grid.tolist()
-    for row in range(self.n_row):
-        for col in range(self.n_col):
-            if rgba_mat[row][col] == self.tree:
-                rgba_mat[row][col] = self.color_tree
-            elif rgba_mat[row][col] == self.empty:
-                rgba_mat[row][col] = self.color_empty
-            elif rgba_mat[row][col] == self.fire:
-                rgba_mat[row][col] = self.color_fire
-            elif rgba_mat[row][col] == self.rock:
-                rgba_mat[row][col] = self.color_rock
-            elif rgba_mat[row][col] == self.lake:
-                rgba_mat[row][col] = self.color_lake
-            else:
-                raise ValueError('Error: Unidentified cell')
-    rgba_mat = np.array(rgba_mat)
-    return rgba_mat
---
+++ gym_cellular_automata/tests/envs/forest_fire/utils/test_render_ca.py
@@ -0,0 +1,41 @@
+import numpy as np
+from gym import spaces
+
+from gym_cellular_automata.envs.forest_fire.utils.render_ca import plot_grid
+from gym_cellular_automata.envs.forest_fire.utils.config import get_forest_fire_config_dict
+
+CONFIG = get_forest_fire_config_dict()
+
+CELL_STATES = CONFIG['cell_states']
+
+ROW = CONFIG['grid_shape']['n_row']
+COL = CONFIG['grid_shape']['n_col']
+
+CELL_TYPE = np.uint8
+
+TITLE = 'My Awesome Plot'
+
+EMPTY = CONFIG['cell_symbols']['empty']
+TREE = CONFIG['cell_symbols']['tree']
+FIRE = CONFIG['cell_symbols']['fire']
+SYMBOLS = EMPTY, TREE, FIRE
+
+COLOR_EMPTY = CONFIG['cell_colors']['empty']
+COLOR_TREE = CONFIG['cell_colors']['tree']
+COLOR_FIRE = CONFIG['cell_colors']['fire']
+
+def test_color_ordering():
+    grid_space = spaces.Box(0, CELL_STATES - 1, shape=(ROW, COL), dtype=CELL_TYPE)
+    grid = grid_space.sample()
+    
+    figure = plot_grid(grid, TITLE)
+    
+    colors_forest = figure.axes[0].images[0].cmap.colors
+    
+    symbols_with_colors = {EMPTY: COLOR_EMPTY, TREE: COLOR_TREE, FIRE: COLOR_FIRE}
+    
+    sorted_keys = sorted(symbols_with_colors)
+       
+    assert symbols_with_colors[sorted_keys[0]] == colors_forest[0]
+    assert symbols_with_colors[sorted_keys[1]] == colors_forest[1]
+    assert symbols_with_colors[sorted_keys[2]] == colors_forest[2]
