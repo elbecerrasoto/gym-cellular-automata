c210964c28c99467069f018631a1b2de223ad183
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2021-03-08 18:37:34 -0700

    Grid Space Grid Space Add and Test Grid Space was added and test. Features: 1. Instantiation from number of cells 'n' 2. Instantiantion from int array 3. Equality overloading 4. In overloading 5. contains 6. sample with custom probs

--- gym_cellular_automata/envs/forest_fire_v1/spaces/grid.py
+++ gym_cellular_automata/envs/forest_fire_v1/spaces/grid.py
@@ -19,42 +19,42 @@ class Grid(Space):
         >>> Grid(values=[-1, 0, 1], shape=(2,2))
 
     """
-    def __init__(self, n=None, values=None, shape=tuple(), probs=None, dtype=CELL_TYPE):
+    def __init__(self, n=None, values=None, shape=None, probs=None, dtype=CELL_TYPE):
         
-        assert shape is not tuple(), "'shape' must be a non-empty tuple."
+        assert shape is not None, "'shape' must be a non-empty tuple."
         assert n is not None or values is not None, "'n' or 'values' must be provided."
-     
+
+        super().__init__(shape, dtype)
+        
         if values is not None:
-            
             self._from_values = True
             
-            self.values = np.array(values, dtype=dtype)
+            self.values = np.unique(np.array(values, dtype=dtype))
             self.n = len(self.values) 
 
         else:
+            self._from_values = False
             
             assert n > 0, "'n' must be a positive integer."
-            self._from_values = False
+            self.n = n
 
             self.values = np.arange(self.n, dtype=dtype)
-        
-        self.size = reduce(mul, self.shape)
-        
+
         uniform = np.repeat(1.0, self.n) / self.n
         self.probs = uniform if probs is None else probs
 
-        super(Grid, self).__init__(shape, dtype)
+        self.size = reduce(mul, self.shape)
 
     def sample(self):        
         
-        return np.random.choice(self.values, self.size, self.probs).reshape(self.shape)
+        return np.random.choice(self.values, self.size, p=self.probs).reshape(self.shape)
 
     def contains(self, x):
         
         if isinstance(x, list):
             x = np.array(x, dtype=self.dtype)
 
-        return {np.unique(x)}.issubset({self.values}) and self.shape == x.shape
+        return set(np.unique(x)).issubset(set(self.values)) and self.shape == x.shape
 
     def __repr__(self):
         if self._from_values:
@@ -64,3 +64,6 @@ class Grid(Space):
         else:
             
             return f"Grid(n={self.n}, shape={self.shape})"
+
+    def __eq__(self, other):
+        return isinstance(other, Grid) and (self.shape == other.shape) and np.all(self.values == other.values)
