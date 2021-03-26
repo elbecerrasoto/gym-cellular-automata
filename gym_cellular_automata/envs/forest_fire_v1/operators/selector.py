15ef0a70bed69dc2b514c3bed5503aea089ea026
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2021-03-25 18:11:49 -0700

    Sequencer Sketch Draft of Operation sequencing.

--- gym_cellular_automata/envs/forest_fire_v1/operators/selector.py
+++ gym_cellular_automata/envs/forest_fire_v1/operators/selector.py
@@ -1,1 +1,27 @@
 # Perception and Induction
+
+# The timing is clearer if it is on the environment.
+
+
+# Temporal Sequencing
+def get_operation_flow(operators, state, action):
+    return ...
+
+
+grid = ...
+operators = ...
+contexts = ...
+action = ...
+
+
+operation_flow = get_operation_flow(operators, action, state=(grid, contexts))
+
+for i_op in operation_flow:
+
+    operator = operators[i_op]
+    context = contexts[i_op]
+
+    new_grid, new_context = operator(grid, action, context)
+
+    grid = new_grid
+    contexts[i_op] = new_context
