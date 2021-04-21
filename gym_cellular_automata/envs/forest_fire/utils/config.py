cd09483afb88311e80d6d4a32665bd718c8dfffd
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2021-04-21 13:32:03 -0600

    :white_check_mark: config utests
    Testing common parsings.

--- gym_cellular_automata/envs/forest_fire/utils/config.py
+++ gym_cellular_automata/envs/forest_fire/utils/config.py
@@ -1,6 +1,4 @@
 # Load the YAML configuration file into a python dict
-
-
 def get_config_dict(file):
     import yaml
 
@@ -9,50 +7,34 @@ def get_config_dict(file):
     return yaml_content
 
 
-# Post YAML loading dynamic modifications
+# Utilities
 # Common parsing operations on Forest Fire CA Environments
+# Post YAML loading, dynamic modifications
+
+# Useful for changing Effects on string to Effects on int representation
+def translate(mapping: dict, translation: dict) -> dict:
+    m = mapping
+    f = translation
+    return {f[x]: f[m[x]] for x in m}
 
 
-def parse_effects(config, kw_effects="effects", kw_cell_symbols="cell_symbols"):
-    effects_str = config["effects"]
-    effects = dict()
+# Group movement actions, useful for instantiate a "move operator"
+def group_actions(actions: dict) -> dict:
+    import re
 
-    for cell_name in effects_str:
-        cell_symbol = config["cell_symbols"][cell_name]
+    def group_byRegex(actions: dict, regex: str) -> set:
+        return {actions[action] for action in actions if re.search(regex, action)}
 
-        substitution_name = effects_str[cell_name]
-        substitution = config["cell_symbols"][substitution_name]
+    REGEXES = (r"^up", r"^down", r"left$", r"right$", r"not_move")
 
-        effects[cell_symbol] = substitution
-
-    return effects
-
-
-def parse_actions(config):
-    up_left = config["actions"]["movement"]["up_left"]
-    up = config["actions"]["movement"]["up"]
-    up_right = config["actions"]["movement"]["up_right"]
-
-    left = config["actions"]["movement"]["left"]
-    not_move = config["actions"]["movement"]["not_move"]
-    right = config["actions"]["movement"]["right"]
-
-    down_left = config["actions"]["movement"]["down_left"]
-    down = config["actions"]["movement"]["down"]
-    down_right = config["actions"]["movement"]["down_right"]
-
-    up_set = {up_left, up, up_right}
-    down_set = {down_left, down, down_right}
-
-    left_set = {up_left, left, down_left}
-    right_set = {up_right, right, down_right}
-
-    not_move_set = {not_move}
+    up, down, left, right, not_move = map(
+        lambda regex: group_byRegex(actions, regex), REGEXES
+    )
 
     return {
-        "up": up_set,
-        "down": down_set,
-        "left": left_set,
-        "right": right_set,
-        "not_move": not_move_set,
+        "up": up,
+        "down": down,
+        "left": left,
+        "right": right,
+        "not_move": not_move,
     }
--- gym_cellular_automata/tests/envs/forest_fire/utils/test_config.py
+++ gym_cellular_automata/tests/envs/forest_fire/utils/test_config.py
@@ -1,6 +1,10 @@
 import pytest
 
-from gym_cellular_automata.envs.forest_fire.utils.config import get_config_dict
+from gym_cellular_automata.envs.forest_fire.utils.config import (
+    get_config_dict,
+    group_actions,
+    translate,
+)
 
 # Test temporal file
 YAML = "test.yaml"
@@ -30,6 +34,7 @@ def content():
 
 
 def test_int_float_str_parsing(tmp_path, content):
+
     path = tmp_path / YAML
     path.write_text(content)
 
@@ -44,3 +49,58 @@ def test_int_float_str_parsing(tmp_path, content):
 
         assert observed == expected
         assert isinstance(observed, type(expected))
+
+
+class TestCommonParsings:
+    @pytest.fixture
+    def mapping(self):
+        L = 12
+        return {key: key for key in range(L)}
+
+    @pytest.fixture
+    def anames(self):
+        return (
+            "up_left",
+            "up",
+            "up_right",
+            "left",
+            "not_move",
+            "right",
+            "down_left",
+            "down",
+            "down_right",
+        )
+
+    @pytest.fixture
+    def anames_per_group(self, anames):
+        return {
+            "up": (anames[0], anames[1], anames[2]),
+            "down": (anames[6], anames[7], anames[8]),
+            "left": (anames[0], anames[3], anames[6]),
+            "right": (anames[2], anames[5], anames[8]),
+            "not_move": (anames[4],),
+        }
+
+    @pytest.fixture
+    def actions(self, anames):
+        return {aname: action for action, aname in enumerate(anames)}
+
+    def test_translate(self, mapping):
+        f = str
+        translation = {key: f(key) for key in mapping}
+        observed = translate(mapping, translation)
+        expected = {f(key): f(key) for key in observed}
+
+        assert all([observed[key] == expected[key] for key in observed])
+
+    def test_group_actions(self, anames, anames_per_group, actions):
+        grouped_actions = group_actions(actions)
+
+        all_in_group = lambda akeys, group: all(
+            [actions[akey] in group for akey in akeys]
+        )
+
+        for group_name in anames_per_group:
+            assert all_in_group(
+                anames_per_group[group_name], grouped_actions[group_name]
+            )
