d0a0dfdea199a145b593b75fcf6e02e44b5c87ee
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2021-05-05 12:48:14 -0600

    :white_check_mark: ca repeat & single pass
    Basic tests of single pass and ca repeat.

--- gym_cellular_automata/envs/forest_fire/bulldozer_v1/operators/ca_repeat.py
+++ gym_cellular_automata/envs/forest_fire/bulldozer_v1/operators/ca_repeat.py
@@ -1,10 +1,30 @@
 import math
-from operator import mul
 from typing import Callable
 
-from collection import namedtuple
-
 from gym_cellular_automata import Operator
+
+
+class SinglePass(Operator):
+
+    grid_dependant = True
+    action_dependant = True
+    context_dependant = True
+
+    def __init__(self, operators, *args, **kwargs):
+
+        super().__init__(*args, **kwargs)
+
+        self.suboperators = tuple(operators)
+
+    def update(self, grid, subactions, subcontexts):
+        subcontexts = list(subcontexts)
+
+        for i, f in enumerate(self.suboperators):
+
+            grid, icontext = f(grid, subactions[i], subcontexts[i])
+            subcontexts[i] = icontext
+
+        return grid, tuple(subcontexts)
 
 
 class RepeatCA(Operator):
@@ -13,43 +33,34 @@ class RepeatCA(Operator):
     action_dependant = True
     context_dependant = True
 
-    SubOperators = namedtuple("SubOperators", ["sequence"])
-
     def __init__(
         self,
-        sequence,
-        tacting: Callable,
-        tperception: Callable,
-        get_subactions: Callable = mul,
+        cellular_automaton,
+        t_acting: Callable,
+        t_perception: Callable,
         *args,
         **kwargs
     ):
 
         super().__init__(*args, **kwargs)
 
-        self.suboperators = self.SubOperators(sequence)
+        self.t_acting = t_acting
+        self.t_perception = t_perception
+
+        self.ca = cellular_automaton
+        self.suboperators = (self.ca,)
 
     def update(self, grid, action, context):
-        nops = len(self.suboperators.sequence)
+        ca_params, accu_time = context
 
-        subcontexts, accu_time = context
-
-        # Repeat the same action
-        subactions = self.get_subactions(action)
-
-        time_action = self.tacting(action)
-        time_state = self.tperception((grid, context))
-
+        time_action = self.t_acting(action)
+        time_state = self.t_perception((grid, context))
         time_taken = time_action + time_state
 
         accu_time += time_taken
-        accu_time, car = math.modf(accu_time)
+        accu_time, repeats = math.modf(accu_time)
 
-        # Operartors order is: Apply CA 'car' times, then the others only once
-        opsflow = int(car) * [0] + [i for i in range(1, nops)]
+        for repeat in range(int(repeats)):
+            grid, ca_params = grid, icontext = self.ca(grid, action, ca_params)
 
-        subcontexts_opsflow = subcontexts, opsflow
-
-        grid, subcontexts = self.sequence(grid, subactions, subcontexts_opsflow)
-
-        return grid, (subcontexts, accu_time)
+        return grid, (ca_params, accu_time)
---
+++ gym_cellular_automata/tests/envs/forest_fire/bulldozer_v1/test_ca_repeat.py
@@ -0,0 +1,134 @@
+from copy import copy
+
+import numpy as np
+import pytest
+from gym import spaces
+
+from gym_cellular_automata.envs.forest_fire.bulldozer_v1.operators.ca_repeat import (
+    RepeatCA,
+    SinglePass,
+)
+from gym_cellular_automata.envs.forest_fire.operators import (
+    Modify,
+    Move,
+    WindyForestFire,
+)
+from gym_cellular_automata.grid_space import Grid
+
+TESTS = 8
+SEQUENCE = 8
+
+EMPTY = 0
+BURNED = 1
+TREE = 3
+FIRE = 25
+
+ROW, COL = 8, 8
+
+
+@pytest.fixture
+def grid_space():
+    return Grid(values=[EMPTY, BURNED, TREE, FIRE], shape=(ROW, COL))
+
+
+@pytest.fixture
+def ca(grid_space):
+    dummy_space = spaces.Discrete(1)
+    return WindyForestFire(
+        EMPTY, BURNED, TREE, FIRE, grid_space=grid_space, action_space=dummy_space
+    )
+
+
+@pytest.fixture
+def operators(grid_space):
+
+    directions = {
+        "up": {0},
+        "down": {1},
+        "left": {2},
+        "right": {3},
+        "not_move": {4},
+    }
+
+    effects = {EMPTY: TREE, FIRE: EMPTY, BURNED: TREE}
+
+    move = Move(
+        directions,
+        grid_space=grid_space,
+        action_space=spaces.Discrete(5),
+        context_space=spaces.MultiDiscrete([ROW, COL]),
+    )
+
+    modify = Modify(
+        effects,
+        grid_space=grid_space,
+        action_space=spaces.Discrete(2),
+        context_space=spaces.MultiDiscrete([ROW, COL]),
+    )
+
+    return move, modify
+
+
+def test_single_pass(operators, grid_space):
+    single_pass = SinglePass(operators)
+
+    grid = grid_space.sample()
+    grid02 = copy(grid)
+
+    op0_action = operators[0].action_space.sample()
+    op0_context = operators[0].context_space.sample()
+
+    op1_action = operators[1].action_space.sample()
+    op1_context = operators[1].context_space.sample()
+
+    subactions = op0_action, op1_action
+    subcontexts = op0_context, op1_context
+
+    subactions02 = copy(op0_action), copy(op1_action)
+    subcontexts02 = copy(op0_context), copy(op1_context)
+
+    def get_expected(grid, operators, subactions, subcontexts):
+
+        for i, f in enumerate(operators):
+            grid, __ = f(grid, subactions[i], subcontexts[i])
+
+        return grid
+
+    obserseved, __ = single_pass(grid, subactions, subcontexts)
+    expected = get_expected(grid02, operators, subactions02, subcontexts02)
+
+    assert np.all(obserseved == expected)
+
+
+def test_repeat_ca(ca):
+
+    grid = ca.grid_space.sample()
+    grid02 = copy(grid)
+
+    # Deterministic params
+    params = ca.context_space.high
+    accu = 0.0
+
+    context = params, accu
+
+    def time_per_action(action):
+        return 1.0
+
+    def time_per_state(state):
+        return 1.0
+
+    repeat_ca = RepeatCA(ca, time_per_action, time_per_state)
+
+    observed, new_context = repeat_ca(grid, None, context)
+
+    __, observedc = new_context
+
+    def get_expected(ca, grid, params):
+        grid, __ = ca(grid, None, params)
+        grid, __ = ca(grid, None, params)
+        return grid
+
+    expected = get_expected(ca, grid02, params)
+
+    assert np.all(observed == expected)
+    assert observedc == 0.0
