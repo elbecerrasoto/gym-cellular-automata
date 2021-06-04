3d7d31a6964131c2c36003771e3d5f4d649e6c6b
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2021-06-04 11:19:47 -0600

    :fire: Coord, Sequence, SinglePass
    Remove Operators:
    + Coordinate
        + Bye. Not general enough. It was tightly coupled with environment.
    + Sequence
        + Not needed it. It could return.
    + SinglePass: Not needed it. It could return.
        + Not needed it. It could return.

--- gym_cellular_automata/forest_fire/operators/__init__.py
+++ gym_cellular_automata/forest_fire/operators/__init__.py
@@ -1,7 +1,5 @@
 from gym_cellular_automata.forest_fire.operators.ca_DrosselSchwabl import ForestFire
 from gym_cellular_automata.forest_fire.operators.ca_windy import WindyForestFire
-from gym_cellular_automata.forest_fire.operators.coordinate import Coordinate
 from gym_cellular_automata.forest_fire.operators.modify import Modify
 from gym_cellular_automata.forest_fire.operators.move import Move
 from gym_cellular_automata.forest_fire.operators.repeat_ca import RepeatCA
-from gym_cellular_automata.forest_fire.operators.sequence import Sequence, SinglePass
