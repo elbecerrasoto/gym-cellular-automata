65033ad11fd6ccbedd7b5137081ea9b010af7ac9
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2021-10-02 07:22:02 -0600

    :white_check_mark: strict flag
    Operator test has strict flag.
    
    If True it forces the definition of the optional arguments.

--- gym_cellular_automata/tests/test_operator.py
+++ gym_cellular_automata/tests/test_operator.py
@@ -6,18 +6,25 @@ def test_operator():
     assert_operator(Identity())
 
 
-def assert_operator(op):
+def assert_operator(op, strict=False):
     from gym.spaces import Space
 
-    def assert_optionals(obj, optional_type, *atts):
+    def assert_optionals(obj, optional, atts, strict=False):
+        """
+        Asserting Optional Types
+        """
         for att in atts:
-            assert hasattr(op, att)
-            if getattr(op, att) is not None:
-                assert isinstance(getattr(op, att), optional_type)
 
-    def assert_update(op):
-        try:
+            gatt = getattr(op, att)
+            assert isinstance(gatt, optional) or gatt is None
 
+            if strict:
+                assert isinstance(
+                    gatt, optional
+                ), f"{att} expected to be {optional} or None./nHowever it is {type(gatt)}"
+
+    def assert_update(op, strict=False):
+        def block():
             grid = op.grid_space.sample()
             action = op.action_space.sample()
             context = op.context_space.sample()
@@ -27,8 +34,16 @@ def assert_operator(op):
             assert op.grid_space.contains(grid)
             assert op.context_space.contains(context)
 
-        except AttributeError:
-            pass
+        if strict:
+
+            block()
+
+        else:
+
+            try:
+                block()
+            except AttributeError:
+                pass
 
     assert isinstance(op, Operator)
 
@@ -39,13 +54,13 @@ def assert_operator(op):
         assert_operator(suop)
 
     assert_optionals(
-        op, bool, "grid_dependant", "action_dependant", "context_dependant"
+        op, bool, ("grid_dependant", "action_dependant", "context_dependant"), strict
     )
-    assert_optionals(op, Space, "grid_space", "action_space", "context_space")
+    assert_optionals(op, Space, ("grid_space", "action_space", "context_space"), strict)
 
-    assert_optionals(op, bool, "deterministic")
+    assert_optionals(op, bool, ("deterministic",), strict)
 
     assert hasattr(op, "update")
     assert callable(op.update)
 
-    assert_update(op)
+    assert_update(op, strict)
